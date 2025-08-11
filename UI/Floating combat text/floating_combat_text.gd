f1d3e44c01bca0237b6c3ea10aab6e28d82b9f1c
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-08-11 22:36:55 +0600

    floating combat text demo working

--- UI/Floating combat text/floating_combat_text.gd
+++ UI/Floating combat text/floating_combat_text.gd
@@ -5,19 +5,13 @@ func show_value(value, travel, duration, spread, crit=false):
 	var movement = travel.rotated(randi_range(-spread/2, spread/2))
 	pivot_offset = size / 2
 	
-	$Tween.interpolate_property(self, "position", 
-		position, position + movement, duration, 
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.interpolate_property(self, "modulate:a",
-			1.0, 0.0, duration,
-			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	var tween : Tween = get_tree().create_tween().set_parallel(true)
+	
+	tween.tween_property(self, "position", position + movement, duration).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "modulate:a", 0, duration).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	
 	if crit:
 		modulate = Color(1, 0, 0)
-		$Tween.interpolate_property(self, "scale",
-			scale*2, scale,
-			0.4, Tween.TRANS_BACK, Tween.EASE_IN)
-	$Tween.tween
-	$Tween.start()
-	
-	#yield($Tween, "tween_all_completed")
-	#queue_free()
+		tween.tween_property(self, "scale", scale, 0.4).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+
+	tween.chain().tween_callback(self.queue_free)
--- UI/Floating combat text/main.gd
+++ UI/Floating combat text/main.gd
@@ -10,8 +10,9 @@ var FCT = preload("res://UI/Floating combat text/Floating combat text.tscn")
 
 func show_value(value, crit=false):
 	var fct = FCT.instantiate()
+	fct.position = Vector2i(124, 124)
 	add_child(fct)
 	fct.show_value(str(value), travel, duration, spread, crit)
 
-func _process(delta: float) -> void:
-	show_value(randi_range(100, 1000))
+func _on_texture_button_pressed() -> void:
+	show_value(randi_range(100, 1000), randi_range(1,2) == 1)
