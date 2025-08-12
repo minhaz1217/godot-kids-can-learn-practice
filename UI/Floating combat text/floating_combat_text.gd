7b969865d1afe547cf0916017e24c65e578f9fa6
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-08-12 22:10:46 +0600

    updated floating text from github code

--- UI/Floating combat text/floating_combat_text.gd
+++ UI/Floating combat text/floating_combat_text.gd
@@ -5,7 +5,7 @@ func show_value(value, travel, duration, spread, crit=false):
 	var movement = travel.rotated(randi_range(-spread/2, spread/2))
 	pivot_offset = size / 2
 	
-	var tween : Tween = get_tree().create_tween().set_parallel(true)
+	var tween : Tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT).set_parallel()
 	
 	tween.tween_property(self, "position", position + movement, duration).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "modulate:a", 0, duration).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
@@ -13,5 +13,6 @@ func show_value(value, travel, duration, spread, crit=false):
 	if crit:
 		modulate = Color(1, 0, 0)
 		tween.tween_property(self, "scale", scale, 0.4).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
-
-	tween.chain().tween_callback(self.queue_free)
+	await tween.finished
+	queue_free()
+	#tween.chain().tween_callback(self.queue_free)
--- UI/Floating combat text/main.gd
+++ UI/Floating combat text/main.gd
@@ -5,7 +5,7 @@ extends Node2D
 var FCT = preload("res://UI/Floating combat text/Floating combat text.tscn")
 
 @export var travel = Vector2(0, -80)
-@export var duration = 2
+@export var duration = 1
 @export var spread = PI/2
 
 func show_value(value, crit=false):
@@ -15,4 +15,4 @@ func show_value(value, crit=false):
 	fct.show_value(str(value), travel, duration, spread, crit)
 
 func _on_texture_button_pressed() -> void:
-	show_value(randi_range(100, 1000), randi_range(1,2) == 1)
+	show_value(randi_range(100, 1000), randi_range(1,4) == 1)
